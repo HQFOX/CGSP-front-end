b4c29fcfbc5598a732a5e6ca0eb138b7e98e8187
Henrique Raposo <henrique.raposo95@gmail.com>
2023-12-07 18:48:33 +0000

    feat(info): add info to pages

--- pages/history.tsx
+++ pages/history.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import type { NextPage } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { PageContainer } from "../components/pageContainer/PageContainer";
-import { Box, Divider } from "@mui/material";
+import { Box, Divider, Typography } from "@mui/material";
 import { useTranslation } from "next-i18next";
 import { ProjectInventory } from "../components/projects/projectInventory/ProjectInventory";
 import { Title } from "../components/Title";
@@ -18,6 +18,11 @@ const History: NextPage<{ projects: Project[] }> = (data) => {
 					{"Histórico de Projetos"}
 				</Title>
 				<Divider />
+				<Typography variant="body2" color="text.secondary">
+					<p>Esta cooperativa tem xxx anos de existência ininterrupta e deu início ao seu trabalhos com a promoção do primeiro programa que foram os primeiros 110 fogos do bairro da Malagueira, projeto do arquiteto Siza Vieira.</p>
+					<p>A cooperativa começou inicialmente com o apoio da Câmara de Évora, que cedia terrenos e projeto, do INH ( atual IHRU) que dava apoio técnico e financeiro.</p>
+					<p>Com a conclusão destes apoios e aproveitando o sucesso e confiança depositada pelos sócios a cooperativa continuou a sua missão de forma independente tendo resistido à crise imobiliária de pós 2008 e inclusive expandido as regiões de atuação.</p>
+				</Typography>
 			</Box>
 			<ProjectInventory projects={data.projects} history/>
 		</PageContainer>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Box, Divider, Typography } from "@mui/material";
+import { Box, Card, CardContent, CardHeader, Divider, Grid, Typography } from "@mui/material";
 
 import type { NextPage } from "next";
 import dynamic from "next/dynamic";
@@ -15,9 +15,9 @@ import styled from "@emotion/styled";
 
 import CGSPCarousel from "../components/carousel/Carousel";
 import logo from "../public/logo.svg";
-import Updates from "../components/updates/Update";
 import theme from "../theme";
 import { Title } from "../components/Title";
+import { Architecture, BookmarkAdd, Construction, Description, Group, MeetingRoom, PersonAdd, Savings } from "@mui/icons-material";
 
 const Map = dynamic(() => import("../components/map/Map"), {
 	ssr: false
@@ -31,7 +31,7 @@ const StyledMain = styled("main")({
 
 
 
-const Home: NextPage<{updates : Update[] }> = ( data ) => {
+const Home: NextPage<{updates : Update[] }> = ( ) => {
 	const { t } = useTranslation(["homepage", "common"]);
 
 	return (
@@ -46,16 +46,143 @@ const Home: NextPage<{updates : Update[] }> = ( data ) => {
 					})}>
 					<Box sx={{ pb: 4 }} id="aboutus">
 						<Title variant="h4" component="h1" fontSize={24}>
-							{t("aboutUsTitle")}
+							Quem Somos?
 						</Title>
 						<Divider />
 					</Box>
 					<Typography variant="body2" color="text.secondary" style={{ whiteSpace: "pre-wrap" }}>
-						{t("aboutUsText")}
+					A Cooperativa Giraldo Sem Pavor é uma cooperativa de construção e habitação sediada em Évora, desde xxxx, que opera na região do Alentejo.
 					</Typography>
+					<Typography variant="body2" color="text.secondary" style={{ whiteSpace: "pre-wrap" }}>
+						<p>Esta instituição emerge da união de vontades e interesses em resolver questões essenciais, como garantir habitação digna para famílias, indivíduos ou grupos ocasionais. A cooperativa procura proporcionar soluções sustentáveis e abrangentes, atendendo às necessidades fundamentais da comunidade.</p>
+						<p>Por conseguinte, o foco principal desta cooperativa é viabilizar aos seus cooperantes o acesso à habitação a custos controlados, resultando em preços mais acessíveis em comparação aos valores de mercado.</p>
+						<p>Dentro desse contexto, comprometemo-nos ativamente a estabelecer parcerias estratégicas com profissionais como arquitetos, empresas de construção e câmaras municipais. Através desta colaboração, procuramos não apenas desenhar, mas também concretizar projetos habitacionais inovadores e acessíveis. Os empreendimentos são cuidadosamente planeados, proporcionando não apenas casas, mas lares, nos quais os nossos cooperantes têm a oportunidade de se inscrever.</p>						
+					</Typography>
+					<div style={{ display: "flex", justifyContent: "center", paddingTop: "20px" }}>
+						<Image src={logo} alt="logo" width={200} height={60} />
+					</div>
 				</Box>
 			</Box>
-			<Box sx={{backgroundColor: theme.bg.main, mt: 20, pt: 4, pb: 8}}>
+			<Box sx={{backgroundColor: theme.palette.secondary.light}}>
+				<Box
+					sx={(theme) => ({
+						[theme.breakpoints.up("md")]: { paddingLeft: theme.spacing(15), paddingRight: theme.spacing(15) },
+						[theme.breakpoints.down("md")]: { paddingLeft: theme.spacing(5), paddingRight: theme.spacing(15) },
+						pt: 6, pb: 6
+					})}>
+					<Box sx={{ pb: 4 }} id="aboutus">
+						<Title variant="h4" component="h1" fontSize={24}>
+							{"Como Funciona?"}
+						</Title>
+						<Divider />
+					</Box>
+					<Typography variant="body2" color="text.secondary" style={{ whiteSpace: "pre-wrap" }}>
+						
+					</Typography>
+					<Grid container columnSpacing={2}>
+						<Grid item xs={1} md={3}>
+							<Card sx={{ border: "1px solid rgb(237, 237, 237)", boxShadow: 0}}>
+								<CardHeader title={<Title >Elaboração do Projeto</Title>}  avatar={<Architecture color="success" />}/>
+								<CardContent>
+									<Typography variant="body2" color="text.secondary">
+										<p>Os terrenos são adquiridos pela cooperativa e é feito um projeto em conjunto com os Arquitetos.</p>
+										<p>O projeto é submetido para aprovação pela Câmara.</p>
+									</Typography>
+								</CardContent>
+							</Card>
+						</Grid>
+						<Grid item xs={1} md={3}>
+							<Card sx={{ border: "1px solid rgb(237, 237, 237)", boxShadow: 0}}>
+								<CardHeader title={<><Title>Inscrição</Title></>} avatar={<PersonAdd color="success"/>}/>
+								<CardContent>
+									<Typography variant="body2" color="text.secondary">
+										<p>Os sócios inscrevem-se na cooperativa para a compra de casa, pagando uma verba de 2.5 € por mês , 25 € de Jóia e 100 € euros de capital social, que será reembolsado  um dia que o sócio desista dessa condição.</p>
+									</Typography>
+								</CardContent>
+							</Card>
+						</Grid>
+						<Grid item xs={1} md={3}>
+							<Card sx={{ border: "1px solid rgb(237, 237, 237)", boxShadow: 0}}>
+								<CardHeader title={<Title>Reserva</Title>} avatar={<BookmarkAdd color="success"/>}/>
+								<CardContent>
+									<Typography variant="body2" color="text.secondary">
+										<p>Quando o projeto de arquitetura é aprovado pela Câmara, os sócios são convidados a consultar o mesmo e a fazerem a sua reserva de habitação, caso assim decidam, mediante o pagamento de uma verba de cerca de 5.000,00€ que será reembolsado caso desistam posteriormente, ou descontado posteriormente no custo da habitação.</p>
+									</Typography>
+								</CardContent>
+							</Card>
+						</Grid>
+						<Grid item xs={1} md={3}>
+							<Card sx={{ border: "1px solid rgb(237, 237, 237)", boxShadow: 0}}>
+								<CardHeader title={<Title>Construção</Title>} avatar={<Construction color="success"/>} />
+								<CardContent>
+									<Typography variant="body2" color="text.secondary">
+										<p>Após aprovação de Loteamento pela respetiva Câmara Municipal, pode começar a fase de construção.</p>
+										<p>Nesta fase são disponibilizadas  atualizações da construção nesta plataforma. É também possível para os cooperantes agendar connosco  visitas guiadas aos locais de construção.</p>
+									</Typography>
+								</CardContent>
+							</Card>
+						</Grid>
+					</Grid>
+				</Box>
+			</Box>
+			<Box sx={{backgroundColor: "rgb(249, 249, 249)"}}>
+				<Box
+					sx={(theme) => ({
+						[theme.breakpoints.up("md")]: { paddingLeft: theme.spacing(15), paddingRight: theme.spacing(15) },
+						[theme.breakpoints.down("md")]: { paddingLeft: theme.spacing(5), paddingRight: theme.spacing(15) },
+						pt: 6, pb: 6
+					})}>
+					<Box sx={{ pb: 4 }} id="aboutus">
+						<Title variant="h4" component="h1" fontSize={24}>
+							Vantagens
+						</Title>
+						<Divider />
+					</Box>
+					<Grid container columnSpacing={2}>
+						<Grid item xs={1} md={3}>
+							<Card sx={{ border: "1px solid rgb(237, 237, 237)", boxShadow: 0}}>
+								<CardHeader title={<Title >Custos Controlados</Title>}  avatar={<Savings color="primary" />}/>
+								<CardContent>
+									<Typography variant="body2" color="text.secondary">
+										<p>Por ser uma cooperativa de habitação e construção podemos beneficiar da taxa reduzida do Imposto sobre o Valor Acrescentado (IVA) e transmitir esse benefício ao cooperante.</p>
+									</Typography>
+								</CardContent>
+							</Card>
+						</Grid>
+						<Grid item xs={1} md={3}>
+							<Card sx={{ border: "1px solid rgb(237, 237, 237)", boxShadow: 0}}>
+								<CardHeader title={<><Title>Transparência</Title></>} avatar={<MeetingRoom color="primary"/>}/>
+								<CardContent>
+									<Typography variant="body2" color="text.secondary">
+										<p>Todo processo pode ser acompanhado pelo cooperante através de atualizações, notificadas por email ou contacto telefónico, nesta plataforma.</p>
+									</Typography>
+								</CardContent>
+							</Card>
+						</Grid>
+						<Grid item xs={1} md={3}>
+							<Card sx={{ border: "1px solid rgb(237, 237, 237)", boxShadow: 0}}>
+								<CardHeader title={<Title>Burocracia Simplificada</Title>} avatar={<Description color="primary"/>}/>
+								<CardContent>
+									<Typography variant="body2" color="text.secondary">
+										<p>O longo e difícil processo burocrático é integralmente tratado pela cooperativa.</p>
+									</Typography>
+								</CardContent>
+							</Card>
+						</Grid>
+						<Grid item xs={1} md={3}>
+							<Card sx={{ border: "1px solid rgb(237, 237, 237)", boxShadow: 0}}>
+								<CardHeader title={<Title>Sem Intermediários</Title>} avatar={<Group color="primary"/>} />
+								<CardContent>
+									<Typography variant="body2" color="text.secondary">
+										<p>Contacto direto com o cliente.</p>
+									</Typography>
+								</CardContent>
+							</Card>
+						</Grid>
+					</Grid>
+				</Box>
+			</Box>
+			<Box sx={{ pt: 4, pb: 8}}>
 				<Box
 					sx={(theme) => ({
 						[theme.breakpoints.up("md")]: { paddingLeft: theme.spacing(15), paddingRight: theme.spacing(15) },
@@ -71,12 +198,6 @@ const Home: NextPage<{updates : Update[] }> = ( data ) => {
 						<Map centerCoordinates={[38.56633674453089, -7.925327404275489]} markers={[ [38.56633674453089, -7.925327404275489] ]}/>
 					</Box>
 				</Box>
-				<div style={{ display: "flex", justifyContent: "center" }}>
-					<Image src={logo} alt="logo" width={200} height={60} />
-				</div>
-			</Box>
-			<Box sx={{backgroundColor: theme.bg.main, mt: 20, display: "flex", justifyContent: "center"}}>
-				<Updates updates={data.updates}/>
 			</Box>
 		</StyledMain>
 	);
