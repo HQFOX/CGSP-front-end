f411c25c1834cf457dbb5852185f8afdb238cab9
Henrique Raposo <henrique.raposo95@gmail.com>
2025-11-01 01:01:38 +0000

    fix: add i18n object

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,10 +1,7 @@
-const path = require('path');
-
 module.exports = {
 	i18n: {
 		locales: ['en', 'pt'],
 		defaultLocale: 'pt',
-		localeDetection: false,
-		localePath: path.resolve('./public/locales')
+		localeDetection: false
 	}
 };
