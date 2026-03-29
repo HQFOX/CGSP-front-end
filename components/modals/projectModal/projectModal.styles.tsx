337dd38374d94ce40800bb3b0cf7bcca1589c4c9
Henrique Raposo <henrique.raposo95@gmail.com>
2026-03-30 00:55:53 +0100

    chore: remove unused files

--- components/modals/projectModal/projectModal.styles.tsx
+++
@@ -1,7 +0,0 @@
-import styled from '@emotion/styled';
-import { IconButton } from '@mui/material';
-
-export const StyledIconButton = styled(IconButton)({
-	alignSelf: 'end',
-	marginLeft: 'auto'
-});
