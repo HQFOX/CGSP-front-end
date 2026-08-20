738192e44e3c401a3abea204b1d628f0e49dc3c3
Henrique Raposo <henrique.raposo95@gmail.com>
2026-08-20 23:09:51 +0100

    chore: fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,11 +72,10 @@ export default defineConfig({
 	],
 
 	/* Run your local dev server before starting the tests */
-	webServer: process.env.CI
-		? undefined
-		: {
-				command: 'npm run start',
-				url: 'http://localhost:3000',
-				reuseExistingServer: !process.env.CI
-			}
+	webServer: {
+		command: 'npm run build && npm run start',
+		url: 'http://localhost:3000',
+		reuseExistingServer: !process.env.CI,
+		timeout: 120 * 1000 // build takes longer, bump the timeout
+	}
 });
